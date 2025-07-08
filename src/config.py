ae1266533c5fd139630cb61cb56acb71efabcf00
Amish Nair <142305652+AMISH2305@users.noreply.github.com>
2025-07-08 17:33:48 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 
 load_dotenv()
 
-API_KEY = os.getenv("BINANCE_API_KEY")
-API_SECRET = os.getenv("BINANCE_API_SECRET")
+API_KEY = st.secrets("BINANCE_API_KEY")
+API_SECRET = st.secrets("BINANCE_API_SECRET")
 
 if not API_KEY or not API_SECRET:
     raise ValueError("❌ API credentials not found in .env file.")
