715d1b7c2d802209029a1d69159490c02145fe4d
Amish Nair <142305652+AMISH2305@users.noreply.github.com>
2025-07-15 16:39:34 +0530

    Update client_setup.py

--- src/client_setup.py
+++ src/client_setup.py
@@ -5,5 +5,4 @@ def get_binance_client(testnet=True):
     client = Client(API_KEY, API_SECRET)
     if testnet:
         client.API_URL = "https://testnet.binancefuture.com/fapi"
-        client.ping()
     return client
