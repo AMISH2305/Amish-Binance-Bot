634d4667b8dbfe15ef17710d56e5dcefe82cd6cd
Amish Nair <142305652+AMISH2305@users.noreply.github.com>
2025-07-15 15:56:37 +0530

    Update client_setup.py

--- src/client_setup.py
+++ src/client_setup.py
@@ -1,5 +1,5 @@
 from binance.client import Client
-from config import API_KEY, API_SECRET
+from src.config import API_KEY, API_SECRET
 
 def get_binance_client(testnet=True):
     client = Client(API_KEY, API_SECRET)
